89bb2aa178297e304d09908fd20e60915b77450d
Robert Gulliksson <rgullik1@volvocars.com>
2025-12-26 14:54:56 +0100

    too slow for part 2

--- day11/main.py
+++ day11/main.py
@@ -1,5 +1,5 @@
 import re
-import random
+import time
 def extract_data(file_path: str) -> dict[str, list[str]]:
     devices: dict[str, list[str]] = {}
     with open(file_path, 'r') as f:
@@ -23,17 +23,43 @@ def find_all_paths(devices: dict[str, list[str]], start: str, target: str) -> li
         if node == target:
             paths.append(path[:])
             continue
-        for nxt in devices.get(node):
-            if nxt in path:
+        for next_node in devices.get(node):
+            if next_node in path:
                 continue
-            stack.append((nxt, path + [nxt]))
+            stack.append((next_node, path + [next_node]))
+    return paths
+
+def find_all_paths_svr_fft_dac_out(devices: dict[str, list[str]], start: str, target: str) -> list[list[str]]:
+    paths: list[list[str]] = []
+    stack: list[tuple[str, list[str]]] = [(start, [])]
+    while stack:
+        node, path = stack.pop()
+        if node == target and 'dac' in path and 'fft' in path:
+            paths.append(path[:])
+            continue
+        if len(path) > 100:
+            continue
+        for next_node in devices.get(node, []):
+            if next_node in path:
+                continue
+            stack.append((next_node, path + [next_node]))
     return paths
 
 def main():
     devices = extract_data('data/realdata')
+    print(devices)
+    #Day 1
+    start = time.time()
     paths = find_all_paths(devices, 'you', 'out')
     print('paths:', paths)
     print('number of paths:', len(paths))
-
+    elapsed = time.time() - start
+    print(f"Time taken: {elapsed:.2f} seconds")
+    #Day 2
+    start = time.time()
+    paths2 = find_all_paths_svr_fft_dac_out(devices, 'svr', 'out')
+    print("number of paths:", len(paths2))
+    elapsed = time.time() - start
+    print(f"Time taken: {elapsed:.2f} seconds")
 if __name__ == '__main__':
     main()
